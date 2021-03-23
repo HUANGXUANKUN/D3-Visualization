9c079ae3e3a5e44162a04121b2dfe8ea8ee98368
HUANG XUAN KUN <hxk123123123@gmail.com>
2021-03-23 22:48:02 +0800

    update part 3

--- A1/Part4/js/heatmap.js
+++ A1/Part4/js/heatmap.js
@@ -1,143 +1,326 @@
+function findRMS(arr) {
+  return Math.pow(
+    arr.reduce(function (acc, pres) {
+      return acc + Math.pow(pres, 2);
+    }) / arr.length,
+    0.5
+  );
+}
+
+// mean
+function findMean(arr) {
+  return (
+    arr.reduce(function (acc, prev) {
+      return acc + prev;
+    }) / arr.length
+  );
+}
+
 async function drawHeatMap() {
-  var margin = { top: 80, right: 25, bottom: 30, left: 40 },
-    width = 450 - margin.left - margin.right,
-    height = 450 - margin.top - margin.bottom;
+  var lPatchWidth = 200;
+  var itemSize = 40,
+    cellSize = itemSize - 3,
+    margin = { top: 50, right: 20, bottom: 120, left: 110 };
 
-  // append the svg object to the body of the page
-  var svg = d3.select("#my_dataviz")
-    .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+  var data;
 
-  //Read the data
+  width = window.innerWidth * 0.9 - margin.right - margin.left,
+    height = 800 - margin.top - margin.bottom;
+
+  var colorScale;
+
+  colorHold = [
+    "#781426",
+    "#C76475",
+    "#EF9FAE",
+    "#ABDB92",
+    "#77B75B",
+    "#2E6E12",
+  ];
+  colorLText = [
+    "< -66%",
+    "-66% to -33%",
+    "-33% to 0%",
+    "0% to 33%",
+    "33% to 66%",
+    "> 66%",
+  ];
+
+  function bandClassifier(val, multiplier) {
+    if (val >= 0) {
+      return Math.floor((val * multiplier) / (0.33 * multiplier)) + 1 > 3
+        ? 3
+        : Math.floor((val * multiplier) / (0.33 * multiplier)) + 1;
+    } else {
+      return Math.floor((val * multiplier) / (0.33 * multiplier)) < -3
+        ? -3
+        : Math.floor((val * multiplier) / (0.33 * multiplier));
+    }
+  }
+
   d3.csv("./data/data.csv", function (rawData) {
-    console.log(rawData);
     const dateParser = d3.timeParse("%Y-%m");
 
-    data = rawData.map(d => {
-      date = dateParser(d.Date);
-      d.Year = date.getYear() + 1900;
-      d.Month = date.getMonth() + 1;
-      d.Value = d.TotalRainfallInMonth;
-      return d;
+    data = rawData.map(function (item) {
+      var newItem = {};
+      var date = dateParser(item.Date);
+      newItem.year = date.getYear() + 1900;
+      newItem.month = date.getMonth() + 1;
+      newItem.value = item.TotalRainfallInMonth;
+      return newItem;
+    });
 
-    })
     console.log(data);
 
-    // Labels of row and columns -> unique identifier of the column called 'group' and 'variable'
-    var myGroups = d3.map(data, function (d) {
-      return d.Month;
-    }).keys()
-
-    var myVars = d3.map(data, function (d) {
-      return d.Year;
-    }).keys()
-
-    // Build X scales and axis:
-    var x = d3.scaleBand()
-      .range([0, width])
-      .domain(myGroups)
-      .padding(0.05);
-    svg.append("g")
-      .style("font-size", 15)
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x).tickSize(0))
-      .select(".domain").remove()
-
-    // Build Y scales and axis:
-    var y = d3.scaleBand()
-      .range([height, 0])
-      .domain(myVars)
-      .padding(0.05);
-    svg.append("g")
-      .style("font-size", 15)
-      .call(d3.axisLeft(y).tickSize(0))
-      .select(".domain").remove()
-
-    // Build color scale
-    var myColor = d3.scaleSequential()
-      .interpolator(d3.interpolateInferno)
-      .domain([1, 100])
-
-    // create a tooltip
-    var tooltip = d3.select("#my_dataviz")
-      .append("div")
-      .style("opacity", 0)
-      .attr("class", "tooltip")
-      .style("background-color", "white")
-      .style("border", "solid")
-      .style("border-width", "2px")
-      .style("border-radius", "5px")
-      .style("padding", "5px")
-
-    // Three function that change the tooltip when user hover / move / leave a cell
-    var mouseover = function (d) {
-      tooltip
-        .style("opacity", 1)
-      d3.select(this)
-        .style("stroke", "black")
-        .style("opacity", 1)
-    }
-    var mousemove = function (d) {
-      tooltip
-        .html("The exact value of<br>this cell is: " + d.Value)
-        .style("left", (d3.mouse(this)[0] + 70) + "px")
-        .style("top", (d3.mouse(this)[1]) + "px")
-    }
-    var mouseleave = function (d) {
-      tooltip
-        .style("opacity", 0)
-      d3.select(this)
-        .style("stroke", "none")
-        .style("opacity", 0.8)
+    invertcolors = 0;
+    // Inverting color scale
+    if (invertcolors) {
+      colorHold.reverse();
     }
 
-    // add the squares
-    svg.selectAll()
-      .data(data, function (d) {
-        return d.group + ':' + d.variable;
+    var x_elements = d3
+        .set(
+          data.map(function (item) {
+            return item.year;
+          })
+        )
+        .values(),
+      y_elements = d3
+        .set(
+          data.map(function (item) {
+            return item.month;
+          })
+        )
+        .values();
+        
+    console.log(x_elements)
+    console.log(y_elements)
+
+    var xScale = d3
+      .scaleBand()
+      .domain(x_elements)
+      .range([0, x_elements.length * itemSize])
+      .paddingInner(20)
+      .paddingOuter(cellSize / 2);
+
+    var xAxis = d3
+      .axisBottom()
+      .scale(xScale)
+      .tickFormat(function (d) {
+        return d;
+      });
+
+    var yScale = d3
+      .scaleBand()
+      .domain(y_elements)
+      .range([0, y_elements.length * itemSize])
+      .paddingInner(0.2)
+      .paddingOuter(0.2);
+
+    var yAxis = d3
+      .axisLeft()
+      .scale(yScale)
+      .tickFormat(function (d) {
+        return d;
+      });
+
+    // Finding the mean of the data
+    var mean = findMean(
+      data.map(function (d) {
+        return +d.value;
       })
+    );
+
+    //setting percentage change for value w.r.t average
+    data.forEach(function (d) {
+      d.perChange = (d.value - mean) / mean;
+    });
+
+    colorScale = d3
+      .scaleOrdinal()
+      .domain([-3, -2, -1, 1, 2, 3])
+      .range(colorHold);
+
+    var rootsvg = d3
+      .select("#heatmap")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom);
+    var svg = rootsvg
+      .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // tooltip
+    tooltip = d3
+      .select("body")
+      .append("div")
+      .style("width", "80px")
+      .style("height", "40px")
+      .style("background", "#C3B3E5")
+      .style("opacity", "1")
+      .style("position", "absolute")
+      .style("visibility", "hidden")
+      .style("box-shadow", "0px 0px 6px #7861A5")
+      .style("padding", "10px");
+    toolval = tooltip.append("div");
+
+    var cells = svg
+      .selectAll("rect")
+      .data(data)
+      .enter()
+      .append("g")
+      .append("rect")
+      .attr("class", "cell")
+      .attr("width", cellSize)
+      .attr("height", cellSize)
+      .attr("y", function (d) {
+        return yScale(d.month);
+      })
+      .attr("x", function (d) {
+        return xScale(d.year) - cellSize / 2;
+      })
+      .attr("fill", function (d) {
+        return colorScale(bandClassifier(d.perChange, 100));
+      })
+      .attr("rx", 3)
+      .attr("ry", 3)
+      .on("mouseover", function (d) {
+        console.log(d);
+        //d3.select(this).attr("fill","#655091");
+        d3.select(this).style("stroke", "orange").style("stroke-width", "3px");
+        d3.select(".trianglepointer")
+          .transition()
+          .delay(100)
+          .attr(
+            "transform",
+            "translate(" +
+              -(
+                lPatchWidth / colorScale.range().length / 2 +
+                colorScale.domain().indexOf(bandClassifier(d.perChange, 100)) *
+                  (lPatchWidth / colorScale.range().length)
+              ) +
+              ",0)"
+          );
+
+        d3.select(".LegText")
+          .select("text")
+          .text(
+            colorLText[
+              colorScale.domain().indexOf(bandClassifier(d.perChange, 100))
+            ]
+          );
+      })
+      .on("mouseout", function () {
+        //d3.select(this).attr('fill', function(d) { return colorScale(window.bandClassifier(d.perChange,100));});
+        d3.select(this).style("stroke", "none");
+        tooltip.style("visibility", "hidden");
+      })
+      .on("mousemove", function (d) {
+        tooltip
+          .style("visibility", "visible")
+          .style("top", d3.event.pageY - 30 + "px")
+          .style("left", d3.event.pageX + 20 + "px");
+
+        console.log(d3.mouse(this)[0]);
+        tooltip
+          .select("div")
+          .html(
+            "<strong>" + d.product + "</strong><br/> " + (+d.value).toFixed(2)
+          );
+      });
+
+    svg
+      .append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+      .selectAll("text")
+      .attr("font-weight", "normal");
+
+    svg
+      .append("g")
+      .attr("class", "x axis")
+      .attr(
+        "transform",
+        "translate(0," + (y_elements.length * itemSize + cellSize / 2) + ")"
+      )
+      .call(xAxis)
+      .selectAll("text")
+      .attr("font-weight", "normal")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.5em")
+      .attr("transform", function (d) {
+        return "rotate(-65)";
+      });
+
+    // Legends section
+
+    legends = svg
+      .append("g")
+      .attr("class", "legends")
+      .attr(
+        "transform",
+        "translate(" +
+          ((width + margin.right) / 2 - lPatchWidth / 2 - margin.left / 2) +
+          "," +
+          (height + margin.bottom - 35 - 20) +
+          ")"
+      );
+
+    // Legend traingle pointer generator
+    var symbolGenerator = d3.symbol().type(d3.symbolTriangle).size(64);
+
+    legends
+      .append("g")
+      .attr("transform", "rotate(180)")
+      .append("g")
+      .attr("class", "trianglepointer")
+      .attr(
+        "transform",
+        "translate(" + -lPatchWidth / colorScale.range().length / 2 + ")"
+      )
+      .append("path")
+      .attr("d", symbolGenerator());
+    //Legend Rectangels
+    legends
+      .append("g")
+      .attr("class", "LegRect")
+      .attr("transform", "translate(0," + 15 + ")")
+      .selectAll("rect")
+      .data(colorScale.range())
       .enter()
       .append("rect")
-      .attr("x", function (d) {
-        return x(d.Month)
+      .attr("width", lPatchWidth / colorScale.range().length + "px")
+      .attr("height", "10px")
+      .attr("fill", function (d) {
+        return d;
       })
-      .attr("y", function (d) {
-        return y(d.Year)
-      })
-      .attr("rx", 4)
-      .attr("ry", 4)
-      .attr("width", x.bandwidth())
-      .attr("height", y.bandwidth())
-      .style("fill", function (d) { return myColor(d.Value) })
-      .style("stroke-width", 4)
-      .style("stroke", "none")
-      .style("opacity", 0.8)
-      .on("mouseover", mouseover)
-      .on("mousemove", mousemove)
-      .on("mouseleave", mouseleave)
-    // })
+      .attr("x", function (d, i) {
+        return i * (lPatchWidth / colorScale.range().length);
+      });
 
-    // Add title to graph
-    svg.append("text")
-      .attr("x", 0)
-      .attr("y", -50)
-      .attr("text-anchor", "left")
-      .style("font-size", "22px")
-      .text("A d3.js heatmap");
+    // legend text
+    legends
+      .append("g")
+      .attr("class", "LegText")
+      .attr("transform", "translate(0,45)")
+      .append("text")
+      .attr("x", lPatchWidth / 2)
+      .attr("font-weight", "normal")
+      .style("text-anchor", "middle")
+      .text(colorLText[0]);
 
-    // Add subtitle to graph
-    svg.append("text")
-      .attr("x", 0)
-      .attr("y", -20)
-      .attr("text-anchor", "left")
-      .style("font-size", "14px")
-      .style("fill", "grey")
-      .style("max-width", 400)
-      .text("A short description of the take-away message of this chart.");
-  })
+    // Heading
+    rootsvg
+      .append("g")
+      .attr("transform", "translate(0,30)")
+      .append("text")
+      .attr("x", (width + margin.right + margin.left) / 2)
+      .attr("font-weight", "bold")
+      .attr("font-size", "22px")
+      .attr("font-family", "Segoe UI bold")
+      .style("text-anchor", "middle")
+      .text("Sales Heatmap");
+  });
 }
 drawHeatMap();
